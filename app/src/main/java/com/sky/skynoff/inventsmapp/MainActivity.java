4a6dbcf0a84cc54b37c671012d90cc97f5ac1cf5
personal <ingcesarsmith@gmail.com>
2016-04-22 11:49:46 -0430

    Cambios en la lista de Inicio hecho con colmentario

--- app/src/main/java/com/sky/skynoff/inventsmapp/MainActivity.java
+++ app/src/main/java/com/sky/skynoff/inventsmapp/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
